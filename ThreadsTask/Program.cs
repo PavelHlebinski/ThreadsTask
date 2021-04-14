efaa1284b61f3213e785ee102ca8dd4a378a4c61
PavelKhlebinski <predramer@gamil.com>
2021-04-14 09:58:05 +0300

    Add project files.

---
+++ ThreadsTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace ThreadsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Thread newThread = new Thread(new ParameterizedThreadStart(Worker.DoWork));
+                newThread.Start(i + 1);
+            }
+        }
+    }
+}
---
+++ ThreadsTask/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsTask
+{
+    class Worker
+    {
+        private static Random _random = new Random();
+        private static int _counter = 0;
+        private static object _sync = new object();
+
+        public static void DoWork(object msg)
+        {
+            lock (_sync)
+            {
+                Thread.Sleep(_random.Next(1, 4) * 300);
+                for (var i = 0; i < 3; i++)
+                {
+                    Console.WriteLine($"Message: {msg}; Counter: {_counter}; Inner iteration number: {i}");
+                }
+                _counter++;
+            }
+        }
+    }
+}
